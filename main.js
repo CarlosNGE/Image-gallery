d3b51692e68abfb4308c57eca3312d85bbee075f
Juan Prieto <jcarlosnge@gmail.com>
2024-05-13 14:27:08 -0700

    test

--- main.js
+++ main.js
@@ -2,28 +2,33 @@
 
 
 
-const filterContainer = document.querySelector(".gallery-filter"),
- galleryItems = document.querySelectorAll(".gallery-item");
+const filterContainer = document.querySelector(".gallery-filter");
 
- filterContainer.addEventListener("click", (event) =>{
-   if(event.target.classList.contains("filter-item")){
-   	 // deactivate existing active 'filter-item'
-   	 filterContainer.querySelector(".active").classList.remove("active");
-   	 // activate new 'filter-item'
-   	 event.target.classList.add("active");
-   	 const filterValue = event.target.getAttribute("data-filter");
-   	 galleryItems.forEach((item) =>{
-       if(item.classList.contains(filterValue) || filterValue === 'all'){
-       	item.classList.remove("hide");
-       	 item.classList.add("show");
-       }
-       else{
-       	item.classList.remove("show");
-       	item.classList.add("hide");
-       }
-   	 });
-   }
- });
+const galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) =>{
+    if(event.target.classList.contains("filter-item")){
+    // disactivate exiting active 'filter-item'
+    filterContainer.querySelector('.active').classList.remove("active");
+
+    // active new 'filter-item'
+    event.target.classList.add("active");
+
+    const filterValue = event.target.getAttribute("data-filter");
+    galleryItems.forEach((item) => {
+        if(item.classList.contains(filterValue) || filterValue === 'all'){
+            item.classList.remove("hide");
+            item.classList.add("show");
+        }
+        else{
+            item.classList.remove("show");
+            item.classList.add("hide");
+        }
+    });
+  }
+})
+
+
 
 
 
