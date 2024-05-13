e674309cf3461acfd6ea9ab9c3c095fb2ce44840
Juan Prieto <jcarlosnge@gmail.com>
2024-05-13 14:40:11 -0700

    test

--- main.js
+++ main.js
@@ -29,6 +29,22 @@ filterContainer.addEventListener("click", (event) =>{
 })
 
 
+// As A Vanilla JavaScript Plugin
+
+var gallery =new SimpleLightbox('.gallery a', {
+    /* options */
+
+});
+
+ 
+
+// As A jQuery Plugin -->
+
+var gallery = $('.gallery a').simpleLightbox({
+
+    /* options */
+
+});
 
 
 
